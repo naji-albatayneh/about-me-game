1845661e777f8c9727ff3e6d3d081fcde92904bd
Jane Smith <example@email.com>
2021-02-10 15:37:56 +0800

    adding some console.log instructions

--- js/app.js
+++ js/app.js
@@ -22,6 +22,7 @@ for(let i = 0; i<questions.length;i++){
       break;
 
     default:
+      //console.log('Please keep the answer to: \'yes\' or \'no\' or \'y\' or \'n\' only');
       alert('Please keep the answer to: \'yes\' or \'no\' or \'y\' or \'n\' only');
     }
   }else{
@@ -55,31 +56,37 @@ for(j = 0; j<4;j++){
   case '3':
   case '4':
   case '10':
+    //console.log('The number you picked is not even close to my lucky number!');
     alert('The number you picked is not even close to my lucky number!');
     break;
 
   case '5':
   case '9':
+    //console.log('The number you picked is close to my lucky number!');
     alert('The number you picked is close to my lucky number!');
     break;
 
   case '6':
   case '8':
+    //console.log('The number you picked is very close to my lucky number!');
     alert('The number you picked is very close to my lucky number!');
     break;
 
   case '7':
     grade = grade + 5;
+    //console.log('The number you picked is my lucky number (7)!');
     alert('Fantastic ' + userName + '! The number you picked is my lucky number (7)! Your total score now is ' + grade + ' Points.');
-    j=5;
+    j=5;//To get out of the loop
     break;
 
   default:
+    //console.log('Please pick a number from the range ( 1 to 10 ) only');
     alert('Please pick a number from the range ( 1 to 10 ) only');
   }
 
 }
 if(j===4){
+  //console.log('You could not guess my lucky number (7)!');
   alert('Oops ' + userName + '! You could not guess my lucky number (7)! Your total score now is ' + grade + ' Points.');
 }
 
@@ -94,6 +101,7 @@ for(k = 0; k<6;k++){
   case 'green':
   case 'yellow':
   case 'while':
+    //console.log('The color you picked is not my fav car color!');
     alert('The color you picked is not my fav car color!');
     break;
 
@@ -101,16 +109,19 @@ for(k = 0; k<6;k++){
   case 'gray':
   case 'black':
     grade = grade + 5;
+    //console.log('The color you picked is one of my fav car colors (blue, gray and black)!');
     alert('Fantastic ' + userName + '! The color you picked is one of my fav car colors (blue, gray and black)! Your total score now is ' + grade + ' Points.');
-    k=7;
+    k=7;// To get out the loop
     break;
 
   default:
+    //console.log('Please pick a color from the list only');
     alert('Please pick a color from the list only');
   }
 
 }
 if(k===7){
+  //console.log('You could not guess any of the colors of my fav car (blue, gray and black)!');
   alert('Oops ' + userName + '! You could not guess any of the colors of my fav car (blue, gray and black)! Your total score now is ' + grade + ' Points.');
 }
 
